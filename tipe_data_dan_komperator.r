296925c0a8cc22bffde6122d3956a93f73b3722f
Poltepop <dewaputuadhitya@gmail.com>
2025-11-29 09:09:24 +0700

    tipe data dan kompreator

---
+++ tipe_data_dan_komperator.r
@@ -0,0 +1,20 @@
+#Unguided 1 Statistika Dasar
+#Nama: I Dewa Putu Adhitya Wiraguna
+#NPM: 250713478
+#Kelas: C
+
+#Nomor 2
+x <- "TRUE"
+y <- TRUE
+z <- 1
+
+#bagian A: menentukan tipe data masing-masing variable
+class(x)
+class(y)
+class(z)
+
+#bagian B: membuat ekspresi logika x dan y menghasilkan TRUE
+identical(as.logical(x), y)
+
+#bagian C: membuat ekpresi perbandingan y dan z menghasilkan false
+identical(y, z)
